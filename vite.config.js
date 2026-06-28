7dd50caccf3e5256a32d96151c179491ac39012b
OGlamozda <oglamozda@example.com>
2026-06-28 21:33:39 +0630

    Switch build output to docs folder

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/og-alpha-hub/',
+build: {
+    outDir: 'docs', // <-- This tells Vite to use the docs folder
+  },
 })
 
